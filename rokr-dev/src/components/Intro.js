2df70ae88a93fa5539d8b508c89a17fb37d049ab
Christian Chow <christian.chow.en.yi@gmail.com>
2022-04-19 22:20:27 +0800

    Format tab spacing

--- rokr-dev/src/components/Intro.js
+++ rokr-dev/src/components/Intro.js
@@ -1,47 +1,47 @@
-import { useEffect } from 'react';
-import { Brand } from './Brand';
-import $ from 'jquery';
+import { useEffect } from "react";
+import { Brand } from "./Brand";
+import $ from "jquery";
 
 export default function Intro(props) {
-    useEffect(function() {
-        if (props.loading) {
-            const intro = $('.intro');
-            const brand = $('.intro--brand');
-            const subtitle = $('.intro--subtitle');
-    
-            $(function() {
-                setTimeout(() => {
-                    brand.addClass('active');
-                }, 400);
-    
-                setTimeout(() => {
-                    brand.removeClass('active');
-                    brand.addClass('fade');
-                }, 3200);
-    
-                setTimeout(() => {
-                    subtitle.removeClass('active');
-                    subtitle.addClass('fade');
-                }, 3400);
-    
-                setTimeout(() => {
-                    intro.css('top', '-100vh');
-                }, 3700);
-            });
-        }
-    }, []);
+  useEffect(function () {
+    if (props.loading) {
+      const intro = $(".intro");
+      const brand = $(".intro--brand");
+      const subtitle = $(".intro--subtitle");
 
-    return (
-        <div className='intro text-center'>
-            <div className='intro--header'>
-                <h1 className='intro--brand'>
-                    <Brand />
-                </h1>
-                <br />
-                <h2 className='intro--subtitle animation-text-wipe animate-in'>
-                    Built by RDO
-                </h2>
-            </div>
-        </div>
-    );
+      $(function () {
+        setTimeout(() => {
+          brand.addClass("active");
+        }, 400);
+
+        setTimeout(() => {
+          brand.removeClass("active");
+          brand.addClass("fade");
+        }, 3200);
+
+        setTimeout(() => {
+          subtitle.removeClass("active");
+          subtitle.addClass("fade");
+        }, 3400);
+
+        setTimeout(() => {
+          intro.css("top", "-100vh");
+        }, 3700);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="intro text-center">
+      <div className="intro--header">
+        <h1 className="intro--brand">
+          <Brand />
+        </h1>
+        <br />
+        <h2 className="intro--subtitle animation-text-wipe animate-in">
+          Built by RDO
+        </h2>
+      </div>
+    </div>
+  );
 }
